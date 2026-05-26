14d6684193ffd2eb8ee4744463e010537d28267a
Dwiki Nuwangsa <74884539+dwikinuwangsa02@users.noreply.github.com>
2026-05-26 14:39:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,27 +7,44 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "Preparation ..."
-apt install unzip -y
+apt update && apt install unzip wget -y
 
-# Latest Stable
+# Menggunakan versi pilihanmu
 CHR_VERSION=7.22.3
 
-# Environment
+# Mengambil informasi sistem otomatis
 DISK=$(lsblk | grep "disk" | head -n 1 | cut -d' ' -f1)
 INTERFACE=$(ip -o -4 route show to default | awk '{print $5}')
 INTERFACE_IP=$(ip addr show $INTERFACE | grep global | cut -d' ' -f 6 | head -n 1)
 INTERFACE_GATEWAY=$(ip route show | grep default | awk '{print $3}')
 
+# Trik anti-corrupt: Jalankan proses dari RAMdisk
+mkdir /tmp/ramdisk
+mount -t tmpfs -o size=256M tmpfs /tmp/ramdisk
+cd /tmp/ramdisk
+
+echo "Downloading MikroTik CHR v${CHR_VERSION}..."
 wget -qO routeros.zip https://download.mikrotik.com/routeros/$CHR_VERSION/chr-$CHR_VERSION.img.zip && \
 unzip routeros.zip && \
 rm -rf routeros.zip
 
-mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt
+# Menyuntikkan konfigurasi IP otomatis agar MikroTik langsung online
+mkdir /mnt/mikrotik
+mount -o loop,offset=512 chr-$CHR_VERSION.img /mnt/mikrotik
 
 echo "/ip address add address=${INTERFACE_IP} interface=[/interface ethernet find where name=ether1]
 /ip route add gateway=${INTERFACE_GATEWAY}
-" > /mnt/rw/autorun.scr
+" > /mnt/mikrotik/rw/autorun.scr
 
-umount /mnt
-echo u > /proc/sysrq-trigger
-dd if=chr-$CHR_VERSION.img of=/dev/${DISK} bs=1M
+umount /mnt/mikrotik
+
+echo "Mengunci disk dan membersihkan cache..."
+sync
+
+# Menulis langsung ke disk utama tanpa interupsi OS
+echo "Writing image to disk /dev/${DISK}..."
+dd if=chr-$CHR_VERSION.img of=/dev/${DISK} bs=4M status=progress && sync
+
+echo "Memicu reboot paksa via Kernel Panic..."
+echo 1 > /proc/sys/kernel/panic
+echo c > /proc/sysrq-trigger
